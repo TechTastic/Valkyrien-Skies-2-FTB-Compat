990161356846d021d88f5c0b507f491bc6f14dd9
TechTastic <74630543+TechTastic@users.noreply.github.com>
2022-12-28 07:17:53 -0600

    Initial Commit
    Both
    -------------------
    Added MixinClaimedChunkManager to pass worldspace position instead of shipyard position to facilitate FTB Chunks protection extending to VS2 ships

--- fabric/src/main/java/org/valkyrienskies/mod/fabric/mixin/compat/ftb_chunks/MixinClaimedChunkManager.java
+++ fabric/src/main/java/org/valkyrienskies/mod/fabric/mixin/compat/ftb_chunks/MixinClaimedChunkManager.java
@@ -4,7 +4,6 @@ import dev.ftb.mods.ftbchunks.data.ClaimedChunk;
 import dev.ftb.mods.ftbchunks.data.ClaimedChunkManager;
 import dev.ftb.mods.ftblibrary.math.ChunkDimPos;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.ChunkPos;
 import org.jetbrains.annotations.Nullable;
@@ -37,15 +36,15 @@ public abstract class MixinClaimedChunkManager {
 
     @Inject(method = "getChunk", at = @At("RETURN"), cancellable = true, remap = false)
     public void ValkyrienSkies$getChunk(final ChunkDimPos dimPos, final CallbackInfoReturnable<ClaimedChunk> cir) {
-        if (entity instanceof ServerPlayer player) {
+        if (entity != null) {
             final ChunkPos chunk = dimPos.getChunkPos();
             final BlockPos pos =
                 new BlockPos(chunk.getMiddleBlockX(), chunk.getWorldPosition().getY(), chunk.getMiddleBlockZ());
-            final Ship ship = VSGameUtilsKt.getShipManagingPos(player.level, pos);
+            final Ship ship = VSGameUtilsKt.getShipManagingPos(entity.level, pos);
             if (ship != null) {
                 final Vector3d vec =
                     ship.getShipToWorld().transformPosition(new Vector3d(pos.getX(), pos.getY(), pos.getZ()));
-                cir.setReturnValue(getChunk(new ChunkDimPos(player.level, new BlockPos(vec.x, vec.y, vec.z))));
+                cir.setReturnValue(getChunk(new ChunkDimPos(entity.level, new BlockPos(vec.x, vec.y, vec.z))));
             }
         }
     }
